3916c6c8cb0537ae6b002e8db68a4d1f95e2d33a
Luiz Claudio <luiz.claudiolc@outlook.com>
2024-10-10 13:47:08 -0300

    feat: configuração de CORS

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,23 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Pipes
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
+      forbidNonWhitelisted: true,
     }),
   );
+
+  // Configurações de CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders:
+      'Content-Type, Accept, Authorization, token, X-Custom-Header',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
